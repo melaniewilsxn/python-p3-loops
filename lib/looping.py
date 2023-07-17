6c1d784a3aec0e667a44315ef09146b6cd2c392d
Melanie Wilson <melaniemwilson1@gmail.com>
2023-07-17 16:59:48 -0400

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i > 0:
+        print(i)
+        if(i == 1):
+            print("Happy New Year!")
+        i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [integer**2 for integer in int_list]
+    return (int_list)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i=1
+    while i <= 100:
+        if (i%3 == 0 and i%5 == 0):
+            print("FizzBuzz")
+        elif (i%3 == 0):
+            print("Fizz")
+        elif (i%5 == 0):
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
